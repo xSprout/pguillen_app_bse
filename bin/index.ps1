de86a1eeb87ce609bc5c5c8ebdf8ca1db51cc680
Pablo <pgguillen@gmail.com>
2018-08-23 23:59:59 -0500

    Windows powershell script support

---
+++ bin/index.ps1
@@ -0,0 +1,7 @@
+$SCRIPT_DIR=(Split-Path $MyInvocation.MyCommand.Path)
+$APP_DIR="$SCRIPT_DIR\.."
+$SPLUNK_HOME="$APP_DIR\..\..\.."
+$NODE_EXEC="$SPLUNK_HOME\bin\node.exe"
+$NODE_SCRIPT="$SPLUNK_HOME\etc\apps\pguillen_app_bse\bin\index.js"
+$PRM="$NODE_SCRIPT", "$SPLUNK_HOME"
+& $NODE_EXEC $PRM
