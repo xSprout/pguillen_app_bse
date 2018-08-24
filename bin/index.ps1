9b3fc32dcd17986dfc7e0d86f7213b332fb58a8d
Pablo <pgguillen@gmail.com>
2018-08-24 11:07:55 -0500

    You need a file monitor with backslash for windows....

--- bin/index.ps1
+++ bin/index.ps1
@@ -4,5 +4,5 @@ $SPLUNK_HOME="$APP_DIR\..\..\.."
 $NODE_EXEC="$SPLUNK_HOME\bin\node.exe"
 $NODE_SCRIPT="$SPLUNK_HOME\etc\apps\pguillen_app_bse\bin\index.js"
 $PRM="$NODE_SCRIPT", "$SCRIPT_DIR"
-echo $SPLUNK_HOME
+# echo $SPLUNK_HOME
 & $NODE_EXEC $PRM
